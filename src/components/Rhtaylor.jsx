07810838fb00085a6fa9f8045f7ce28801ab3f7f
Randy <randyisbusy@gmail.com>
2020-08-09 12:03:02 -0700

    add info to the Rtaylor component

--- src/components/Rhtaylor.jsx
+++ src/components/Rhtaylor.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-
+import {iconfinder_medium_3721675.png} from '../css/iconfinder_medium_3721675'
 export default class Rhtaylor extends Component{
     render(){ 
         debugger
         return(
             <div>
-                <h1>Randall Taylor</h1>
+                <h1>Randall Taylor</h1> 
+                <span><a href="https://medium.com/@software_for_humanity">Blog</a></span> 
+                <p></p>
+                <p>graduated from the University of Arizona with  BS in Physiology, minor in Biochemistry, and second minor in business administration. With a strong interest in science he always had a full mind of scientific and engineering ideas.</p>
             </div>
         )
     }
-}
+} 
+
