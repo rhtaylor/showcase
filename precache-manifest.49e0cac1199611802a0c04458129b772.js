0f6bb9009e4d62fe98bf248255ae1ffd48140dde
Randy <randyisbusy@gmail.com>
2020-08-08 18:24:14 -0700

    Updates

---
+++ precache-manifest.49e0cac1199611802a0c04458129b772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03deafa9aa20b4ce69ce5195b961052",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db80de3926decae37e67",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7971be196eb6c86b1a0",
+    "url": "./static/js/2.1110ce4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1110ce4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db80de3926decae37e67",
+    "url": "./static/js/main.f14344ca.chunk.js"
+  },
+  {
+    "revision": "083cadc3f936c31c2a5c",
+    "url": "./static/js/runtime-main.d47e3dba.js"
+  }
+]);
