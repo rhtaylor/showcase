1f2db37d940e5dc8bb58420f267a8b69355b3174
Randy <randyisbusy@gmail.com>
2020-08-08 19:09:50 -0700

    adds /showcase to /showcase/Rhtaylor

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
     <Route path="/" component={App} />
-    <Route exact path="/Rhtaylor" render={(...routerProps)=> <Rhtaylor {...routerProps} />} />
+    <Route exact path="/showcase/Rhtaylor" render={(...routerProps)=> <Rhtaylor {...routerProps} />} />
   </Router>,
   document.getElementById('root')
 );
