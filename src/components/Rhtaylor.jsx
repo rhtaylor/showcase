28551bbf6c267e2fbc3aa7256aacaa45dc81d00a
Randy <randyisbusy@gmail.com>
2020-08-11 09:44:57 -0700

    adds medium icons to Profile and Portfolio pages

--- src/components/Rhtaylor.jsx
+++ src/components/Rhtaylor.jsx
@@ -7,7 +7,8 @@ export default class Rhtaylor extends Component{
             <div>
                
                 <h1>Randall Taylor</h1> 
-                <span><a href="https://medium.com/@software_for_humanity">Blog</a></span> 
+                <span><a href="https://medium.com/@software_for_humanity" target="_blank"><i className="fab fa-medium"></i></a>
+</span> 
                 <p></p>
                 <p>graduated from the University of Arizona with  BS in Physiology, minor in Biochemistry, and second minor in business administration. With a strong interest in science he always had a full mind of scientific and engineering ideas.</p>
             </div>
