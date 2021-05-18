9baf52ed2a9abbdc2a21e82dd0b84eb788f66341
Randy <randyisbusy@gmail.com>
2021-05-17 22:22:34 -0700

    updates bio page

--- src/components/Rhtaylor.jsx
+++ src/components/Rhtaylor.jsx
@@ -14,11 +14,11 @@ export default class Rhtaylor extends Component{
 </span> 
                 
                 <p>I have a Science degree from the University of Arizona in Physiology with minors in Biochemistry and Business Administration.</p>
-                <p>After working within Emergency Medical Services as a 911 response EMT I learned to implement High Reliability Organization and teamwork to achieve the best outcomes for the sick and injured.</p>
-                <p>EMS was the beginning of my nascent professional career.</p>  
-                <p>Later I moved into Behavioral Health, worked as a BHT, and Case Manager.</p> 
-                <p>A multifacited mix of life experences lead me to want to pursue Software Engineer. I then taught myself JavaScript from codecademy and really enjoyed functional programming. Two years from the date I decided software was the place I belonged and I enrolled in the software bootcamp. 
-                Flatiron taught me the fundamentals of Software Design, debugging, and how to teach myself computer languages professionally. While enrolled I build Two Applications I was extremely proud of: Boiling Forest and PatiensHub. These are detailed on my Projects page.</p>
+                <p>While working within Emergency Medical Services as a 911 response EMT I learned to implement High Reliability Organization and teamwork to achieve the best outcomes for the sick and injured.</p>
+                
+                <p>Later I moved into Behavioral Health Care, worked as a Behavior Health Tech, and Case Manager.</p> 
+                <p>A multifacited mix of life experences lead me to Software Engineering. I then taught myself JavaScript from codecademy and really enjoyed functional programming. Two years from the date I decided on software I enrolled in the software engineering boot camp. 
+                Flatiron taught me the fundamentals of Software Design, debugging, and how to teach myself computer languages professionally. While enrolled I build many Applications, two of which I was extremely proud of: Boiling Forest and PatiensHub. These are detailed on my Projects page.</p>
                 <p>Currently I am studying Artificial intelligence and Machine Learning at the University of Texas</p>
             </div>
         )
